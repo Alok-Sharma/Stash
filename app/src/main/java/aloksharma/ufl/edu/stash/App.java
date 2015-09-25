4e16b49effad80fb5a817a8aaf9fc21354f9a5b5
Alok-Sharma <alok.sharma127@gmail.com>
2015-09-25 18:45:02 -0400

    Testing Plaid with simple REST calls.

--- app/src/main/java/aloksharma/ufl/edu/stash/App.java
+++ app/src/main/java/aloksharma/ufl/edu/stash/App.java
@@ -3,14 +3,11 @@ package aloksharma.ufl.edu.stash;
 import android.app.Application;
 
 import com.parse.Parse;
-import com.plaid.client.PlaidClients;
-import com.plaid.client.PlaidUserClient;
 
 /**
  * Created by Alok on 9/23/2015.
  */
 public class App extends Application {
-    PlaidUserClient plaidUserClient;
     @Override
     public void onCreate() {
         super.onCreate();
@@ -18,8 +15,5 @@ public class App extends Application {
         //Parse
         Parse.enableLocalDatastore(this);
         Parse.initialize(this, getString(R.string.parse_applicationId), getString(R.string.parse_clientKey));
-
-        //Plaid
-        plaidUserClient = PlaidClients.testUserClient("test_id", "test_secret");
     }
 }
--- app/src/main/java/aloksharma/ufl/edu/stash/MainActivity.java
+++ app/src/main/java/aloksharma/ufl/edu/stash/MainActivity.java
@@ -1,13 +1,23 @@
 package aloksharma.ufl.edu.stash;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 
-import com.plaid.client.request.Credentials;
-import com.plaid.client.response.Transaction;
-import com.plaid.client.response.TransactionsResponse;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends Activity {
@@ -17,10 +27,49 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         App app = (App)getApplicationContext(); //Get all variables in App.java
+        new makePostRequest().execute();
+    }
 
-        Credentials testCredentials = new Credentials("plaid_test", "plaid_good");
-        TransactionsResponse response = app.plaidUserClient.addUser(testCredentials, "amex", "test@test.com", null);
-        List<Transaction> transactions = response.getTransactions();
-        Log.d("Alok", "" + transactions);
+    private class makePostRequest extends AsyncTask{
+
+        @Override
+        protected Object doInBackground(Object[] params) {
+            HttpClient httpClient = new DefaultHttpClient();
+            // replace with your url
+            HttpPost httpPost = new HttpPost("https://tartan.plaid.com/connect");
+
+            //Post Data
+            List<NameValuePair> nameValuePair = new ArrayList<NameValuePair>();
+            nameValuePair.add(new BasicNameValuePair("client_id ", "test_id"));
+            nameValuePair.add(new BasicNameValuePair("secret", "test_secret"));
+            nameValuePair.add(new BasicNameValuePair("username", "plaid_test"));
+            nameValuePair.add(new BasicNameValuePair("password", "plaid_good"));
+            nameValuePair.add(new BasicNameValuePair("type", "bofa"));
+
+
+            //Encoding POST data
+            try {
+                httpPost.setHeader("Content-type", "application/json");
+                httpPost.setHeader("Accept", "application/json");
+                httpPost.setEntity(new UrlEncodedFormEntity(nameValuePair));
+            } catch (UnsupportedEncodingException e) {
+                // log exception
+                e.printStackTrace();
+            }
+
+            //making POST request.
+            try {
+                HttpResponse response = httpClient.execute(httpPost);
+                // write response to log
+                Log.d("nikita", EntityUtils.toString(response.getEntity()));
+            } catch (ClientProtocolException e) {
+                // Log exception
+                e.printStackTrace();
+            } catch (IOException e) {
+                // Log exception
+                e.printStackTrace();
+            }
+            return null;
+        }
     }
 }
