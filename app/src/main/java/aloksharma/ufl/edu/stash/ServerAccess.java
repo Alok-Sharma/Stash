aaa666dc9fd3af17a5897768ce87dc48d3cb2466
Alok-Sharma <alok.sharma127@gmail.com>
2015-10-15 23:04:42 -0400

    Restrucutred plaid code into a class of its own.

--- app/src/main/java/aloksharma/ufl/edu/stash/ServerAccess.java
+++ app/src/main/java/aloksharma/ufl/edu/stash/ServerAccess.java
@@ -4,35 +4,18 @@ import android.app.IntentService;
 import android.content.Intent;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
-import android.widget.ArrayAdapter;
 
-import com.parse.FindCallback;
-import com.parse.ParseACL;
-import com.parse.ParseException;
-import com.parse.ParseObject;
-import com.parse.ParseQuery;
-import com.parse.ParseRelation;
 import com.facebook.android.crypto.keychain.SharedPrefsBackedKeyChain;
 import com.facebook.crypto.Crypto;
 import com.facebook.crypto.Entity;
 import com.facebook.crypto.util.SystemNativeCryptoLibrary;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.util.EntityUtils;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -82,16 +65,17 @@ public class ServerAccess extends IntentService {
             case GET_BALANCE:
                 //Make appropriate getBankBalance call depending if
                 // username/password is available in the intent.
+                PlaidHelper plaidHelper = new PlaidHelper(this);
                 String bankUsername = incomingIntent.getStringExtra
                         ("bankUsername");
                 if (bankUsername == null) {
                     //no username, password. Use access token.
-                    Map<String, byte[]> accessTokens = getAccessTokenMap();
+                    Map<String, byte[]> accessTokens = plaidHelper.getAccessTokenMap();
                     try {
                         byte[] encryptedFetch = accessTokens.get("wells");
                         byte[] decryptedFetch = crypto.decrypt(encryptedFetch, new Entity("password")); //TODO: use the users password to encrypt and decrypt.
                         String accessToken = new String(decryptedFetch);
-                        Double balance = getBankBalance(accessToken);
+                        Double balance = plaidHelper.getBankBalance(accessToken);
                         Log.d("StashLog", "balance: " + balance);
                         outgoingIntent.putExtra("balance", balance);
                     } catch (Exception e) {
@@ -103,7 +87,7 @@ public class ServerAccess extends IntentService {
                             ("bankPassword");
                     String bankName = incomingIntent.getStringExtra
                             ("bankName");
-                    Double balance = getBankBalance(bankUsername,
+                    Double balance = plaidHelper.getBankBalance(bankUsername,
                             bankPassword, BankName.valueOf(bankName));
                     Log.d("StashLog", "balance: " + balance);
                     outgoingIntent.putExtra("balance", balance);
@@ -121,24 +105,6 @@ public class ServerAccess extends IntentService {
 
     public void getUser() {
         //Stub to get the Parse user object.
-    }
-
-    private Double getBankBalance(String access_token) {
-        List<NameValuePair> postArgs = new ArrayList();
-        String plaidUrl = getString(R.string.plaid_url) + "/get";
-        postArgs.add(new BasicNameValuePair("access_token", access_token));
-
-        return plaidPostRequest(plaidUrl, postArgs);
-    }
-
-    private Double getBankBalance(String username, String password, BankName
-            bankName) {
-        List<NameValuePair> postArgs = new ArrayList();
-        String plaidUrl = getString(R.string.plaid_url);
-        postArgs.add(new BasicNameValuePair("username", username));
-        postArgs.add(new BasicNameValuePair("password", password));
-        postArgs.add(new BasicNameValuePair("type", bankName.toString()));
-        return plaidPostRequest(plaidUrl, postArgs);
     }
 
    /******************Add Stash Functionality******************************************/
@@ -233,31 +199,7 @@ public class ServerAccess extends IntentService {
     }
     /******************End of Remove Stash Functionality*********************************************/
 
-
-
-
-    /**
-     * Returns the Map of the Bank access tokens. This map has a key of the bank name
-     * and the values are the encrypted access tokens.
-     * @return Map<String, byte[]> of access tokens.
-     */
-    private Map<String, byte[]> getAccessTokenMap() {
-        try {
-            if (ParseUser.getCurrentUser() != null) {
-                Map<String, byte[]> accessTokens = ParseUser.getCurrentUser().getMap("BankMap");
-                return accessTokens;
-            }else{
-                //current user is null. This shouldn't happen if the user was
-                // logged in successfully.
-                Log.d("StashLog", "current user was null");
-            }
-        }catch(Exception e){
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    private void addAccessToken() {
+//    private void addAccessToken() {
 //        String input = "test_wells";
 //        try {
 //            byte[] encryptedInput = crypto.encrypt(input.getBytes(), new Entity("password"));
@@ -269,60 +211,5 @@ public class ServerAccess extends IntentService {
 //        } catch (Exception e) {
 //            e.printStackTrace();
 //        }
-    }
-
-    /**
-     * Executes a POST to the plaid /auth endpoint, and fetches the users
-     * bank balance.
-     *
-     * @param plaidUrl The Plaid url to send the request to
-     * @param postArgs The POST arguments
-     * @return Double Users Bank balance.
-     */
-    private Double plaidPostRequest(String plaidUrl, List<NameValuePair>
-            postArgs) {
-        HttpClient httpClient = new DefaultHttpClient();
-        HttpPost httpPost = new HttpPost(plaidUrl);
-
-        //Post Data
-        postArgs.add(new BasicNameValuePair("client_id", getString(R.string
-                .plaid_client_id)));
-        postArgs.add(new BasicNameValuePair("secret", getString(R.string
-                .plaid_secret)));
-
-        //Encoding POST data
-        try {
-            httpPost.setEntity(new UrlEncodedFormEntity(postArgs));
-        } catch (UnsupportedEncodingException e) {
-            // log exception
-            e.printStackTrace();
-        }
-
-        //making POST request, and parse it.
-        try {
-            HttpResponse response = httpClient.execute(httpPost);
-            String responseString = EntityUtils.toString(response.getEntity());
-            JSONObject jObject = new JSONObject(responseString);
-            Log.d("StashLog", "PLAID RESPONSE: " + responseString);
-
-            //fetch the access_token and store it on parse.
-            String access_token = jObject.getString("access_token");
-            //currentUser must not be null.
-            ParseUser.getCurrentUser().addUnique("access_tokens",
-                    access_token);
-            ParseUser.getCurrentUser().pinInBackground();
-            ParseUser.getCurrentUser().saveInBackground();
-
-            double balance = jObject.getJSONArray("accounts").getJSONObject
-                    (0).getJSONObject("balance").getDouble("available");
-            return balance;
-        } catch (ClientProtocolException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
+//    }
 }
