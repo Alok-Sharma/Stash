35aba7f042df92aebb82fe8cae643a2e96c26225
Nikita Dagar <ndagar@ufl.edu>
2015-11-07 00:06:43 -0500

    Bank Names Enum part done

--- app/src/main/java/aloksharma/ufl/edu/stash/AccountActivity.java
+++ app/src/main/java/aloksharma/ufl/edu/stash/AccountActivity.java
@@ -10,6 +10,8 @@ import android.view.View;
 
 import com.parse.ParseUser;
 
+import java.util.HashMap;
+
 public class AccountActivity extends ActionBarActivity {
 
     @Override
@@ -20,6 +22,9 @@ public class AccountActivity extends ActionBarActivity {
 
         final Intent serverIntent = new Intent(this, ServerAccess.class);
 
+        final HashMap<String, Enum> bankNamesHash = new HashMap<String, Enum>();
+        createBankNamesHash(bankNamesHash);
+
         Button add_account = (Button) findViewById(R.id.add_account_btn);
         add_account.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -28,16 +33,32 @@ public class AccountActivity extends ActionBarActivity {
                 Spinner bankNamesList = (Spinner) findViewById(R.id.bankNamesList);
                 String bank = bankNamesList.getSelectedItem().toString();
 
-
                 serverIntent.putExtra("server_action", ServerAccess.ServerAction.GET_BALANCE.toString());
                 serverIntent.putExtra("bankUsername", bankUsername.getText().toString());
                 serverIntent.putExtra("bankPassword", bankPassword.getText().toString());
-                serverIntent.putExtra("bankName", bank);
+                serverIntent.putExtra("bankName", bankNamesHash.get(bank).toString());
                 startService(serverIntent);
-
             }
         });
 
+    }
+
+    private void createBankNamesHash(HashMap<String, Enum> hm){
+        hm.put("American Express", ServerAccess.BankName.amex);
+        hm.put("Bank of America", ServerAccess.BankName.bofa);
+        hm.put("Capital One", ServerAccess.BankName.capone360);
+        hm.put("Charles Schwab", ServerAccess.BankName.schwab);
+        hm.put("Chase", ServerAccess.BankName.chase);
+        hm.put("Citi", ServerAccess.BankName.citi);
+        hm.put("Fidelity", ServerAccess.BankName.fidelity);
+        hm.put("Navy Federal Credit Union", ServerAccess.BankName.nfcu);
+        hm.put("PNC", ServerAccess.BankName.pnc);
+        hm.put("Silicon Valley Bank", ServerAccess.BankName.svb);
+        hm.put("SunTrust", ServerAccess.BankName.suntrust);
+        hm.put("TD Bank", ServerAccess.BankName.td);
+        hm.put("US Bank", ServerAccess.BankName.us);
+        hm.put("USAA", ServerAccess.BankName.usaa);
+        hm.put("Wells Fargo", ServerAccess.BankName.wells);
     }
 
 }
