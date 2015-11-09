a4811158cbafe63d229cb3d9ce6e1802bb3e4dc4
kkhetarpal <khimyakhetarpal@gmail.com>
2015-11-09 00:35:10 -0700

    Profile Activity -UI - Phase I

---
+++ app/src/main/java/aloksharma/ufl/edu/stash/ProfileActivity.java
@@ -0,0 +1,19 @@
+package aloksharma.ufl.edu.stash;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+/**
+ * Created by khimya on 11/8/15.
+ */
+public class ProfileActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+
+
+}
