f6fc152c214d0e1d3019b15cb5703586b7ced5a6
pritic <changlani.priti@gmail.com>
2015-11-21 22:08:21 -0500

    drawer name bug now fixed

--- app/src/main/java/aloksharma/ufl/edu/stash/DrawerActivity.java
+++ app/src/main/java/aloksharma/ufl/edu/stash/DrawerActivity.java
@@ -50,7 +50,7 @@ public class DrawerActivity extends AppCompatActivity {
         currentUser = ParseUser.getCurrentUser();
         Log.d("Stash: DrawerActivity", "CurrentUser is: " + currentUser
                 .getUsername());
-        NAME = currentUser.getString("username");
+        NAME = currentUser.getUsername();
         Log.d("Stash: Drawer Activity:", "User's Name: " + NAME);
         EMAIL = currentUser.getEmail();
         Log.d("Stash: Drawer Activity:", "User's Email: " + EMAIL);
